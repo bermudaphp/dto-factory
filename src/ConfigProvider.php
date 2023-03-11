6674442da4e9dece1d5434b739353ef0b606d3f8
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2023-03-11 22:26:06 +0200

    Create ConfigProvider.php

---
+++ src/ConfigProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Bermuda\DtoFactory;
+
+use Psr\Container\ContainerInterface;
+
+class ConfigProvider extends \Bermuda\Config\ConfigProvider
+{
+    const configKey = 'Bermuda\DtoFactory';
+    const factoriesConfigKey = 'factories';
+
+    protected function getAliases(): array
+    {
+        return [DtoFactoryInterface::class => DtoFactory::class];
+    }
+
+    protected function getFactories(): array
+    {
+        return [DtoFactory::class => static function(ContainerInterface $c) {
+            $config = conf($c);
+            $factory = new DtoFactory();
+            if (
+                $config->offsetExists(self::configKey) 
+                && $config->offsetExists(self::factoriesConfigKey)
+            ) foreach ($config[self::configKey][self::factoriesConfigKey] as $dtoFactory) {
+                $factory->addFactory($dtoFactory);
+            }
+            
+            return $factory;
+       }];
+    }
+}
